fa62fee1d2b23690608b0ec4a912f8b1e1eb4a93
wheatwaves <wheatwaves@SenmatoMBP.hsd1.ca.comcast.net>
2021-11-04 00:20:30 -0700

    update

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
+const Wallet = artifacts.require("Wallet");
+
+module.exports = async function (deployer, _network, accounts) {
+  await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
+  const wallet = await Wallet.deployed();
+  web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
+};
